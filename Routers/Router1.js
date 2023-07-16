c261509a2e8f14da34d9e1159660ef78f13306d2
subhendu20 <64896535+subhendu20@users.noreply.github.com>
2023-07-16 23:43:16 +0530

    first commit

---
+++ Routers/Router1.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const cors = require('cors')
+const cookie = require('cookie-parser')
+
+
+
+
+
+
+
+{
+          (areaprofiles.list.length!==0) && 
+        }
+
+
+<span><img src={img} alt="loading" /> subhidha</span>
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+const app = express()
+const middleware = require('body-parser')
+const routelog = require('./Routers/Routerforlogs')
+const routepost = require('./Routers/Routerforpost')
+const routecomment = require('./Routers/Routerforcomment')
+const routeevent = require('./Routers/Routerforevent') 
+const port = process.env.PORT || 7000
+const path = require('path')
+
+// middlewares
+app.use(middleware.urlencoded({ extended: false }));
+app.use(middleware.json());
+app.use('/users',routelog)
+app.use('/post',routepost)
+app.use('/comment',routecomment)
+app.use('/events',routeevent)
+
+
+
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.use('*',(req,res)=>{
+          res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
+// dbconnection
+mongoose.connect(process.env.DB).then((e)=>{
+          console.log("connected to database")
+}).catch((e)=>{
+          console.log("nt")
+})
+
+
+
+// port setting
+app.listen(port,()=>{
+          console.log(`App running on ${process.env.PORT}`)
+})
+
