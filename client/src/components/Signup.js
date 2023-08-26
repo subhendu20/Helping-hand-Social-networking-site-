3b7dd629672d933957ac700d6313584faba5177b
subhendu20 <64896535+subhendu20@users.noreply.github.com>
2023-08-26 14:23:18 +0530

    new

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -89,12 +89,7 @@ function Signup() {
     let vh = window.innerHeight * 0.01;
   
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  window.addEventListener('resize', function () {
-    vh = window.innerHeight * 0.01;
   
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    
-    });
     window.addEventListener('orientationchange', function () {
       vh = window.innerHeight * 0.01;
   
